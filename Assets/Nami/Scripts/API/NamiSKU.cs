5d6f00c3c4d10f5f2cfe10e1bfbf4e28f427b8e6
Yaroslav Stadnyk <yaroslavsart@gmail.com>
2023-02-25 03:44:02 +0200

    nami sku json constructor

--- Assets/Nami/Scripts/API/NamiSKU.cs
+++ Assets/Nami/Scripts/API/NamiSKU.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using NamiSdk.MiniJSON;
 using NamiSdk.Utils;
 using UnityEngine;
 
@@ -13,11 +15,32 @@ namespace NamiSdk
             Type = ajo.CallAJO("getType").JavaToEnum<NamiSKUType>("_");
         }
 
-        public string Name { get; private set; }
-        public string SkuId { get; private set; }
-        public string SkuDetails { get; private set; }  // TODO check out SkuDetails class
-        public NamiSKUType Type { get; private set; }
+        public NamiSKU(string json)
+        {
+            if (Json.Deserialize(json) is Dictionary<string, object> jsonDictionary)
+            {
+                jsonDictionary.TryGetValue("name", out var nameObject);
+                jsonDictionary.TryGetValue("skuId", out var skuIdObject);
+                jsonDictionary.TryGetValue("product", out var productObject);
+                jsonDictionary.TryGetValue("type", out var typeObject);
 
-        // TODO implement product variable for IOS (SkuDetails is working for Android only)
+                Name = (string)nameObject;
+                SkuId = (string)skuIdObject;
+                Product = (string)productObject;
+                if (typeObject != null) Type = (NamiSKUType)typeObject;
+            }
+        }
+
+        public string Name { get; private set; }
+
+        public string SkuId { get; private set; }
+
+        /// <summary> Android platforms only </summary>
+        public string SkuDetails { get; private set; }  // TODO check out SkuDetails class
+
+        /// <summary> Apple platforms only </summary>
+        public string Product { get; private set; }  // TODO check out Product class
+
+        public NamiSKUType Type { get; private set; }
     }
 }

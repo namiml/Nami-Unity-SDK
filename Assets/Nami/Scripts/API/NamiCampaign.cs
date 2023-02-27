d516f2f0c7ae0e006c21436d9fae646d50516f32
Yaroslav Stadnyk (Apple) <yaroslavsart@gmail.com>
2023-02-27 19:22:31 +0200

    nami campaign json deserialization

--- Assets/Nami/Scripts/API/NamiCampaign.cs
+++ Assets/Nami/Scripts/API/NamiCampaign.cs
@@ -1,3 +1,4 @@
+using NamiSdk.MiniJSON;
 using NamiSdk.Utils;
 using UnityEngine;
 
@@ -13,9 +14,40 @@ namespace NamiSdk
             Value = ajo.CallStr("getValue");;
         }
 
+        public NamiCampaign(object json)
+        {
+            var dictionary = Json.DeserializeDictionary(json);
+            if (dictionary != null)
+            {
+                dictionary.TryGetValue("id", out var idObject);
+                dictionary.TryGetValue("rule", out var ruleObject);
+                dictionary.TryGetValue("paywall", out var paywallObject);
+                dictionary.TryGetValue("segment", out var segmentObject);
+                dictionary.TryGetValue("type", out var typeObject);
+                dictionary.TryGetValue("value", out var valueObject);
+
+                Id = (string)idObject;
+                Rule = (string)ruleObject;
+                Paywall = (string)paywallObject;
+                Segment = (string)segmentObject;
+                if (typeObject != null) Type = (NamiCampaignRuleType)(long)typeObject;
+                Value = (string)valueObject;
+            }
+        }
+
+        /// <summary> Apple platforms only </summary>
+        public string Id { get; private set; }
+
+        /// <summary> Apple platforms only </summary>
+        public string Rule { get; private set; }
+
         public string Paywall { get; private set; }
+
         public string Segment { get; private set; }
+
+        /// <summary> Android platforms only </summary>
         public NamiCampaignRuleType Type { get; private set; }
+
         public string Value { get; private set; }
     }
 }
